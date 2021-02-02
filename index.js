0a7bcf01b3865a9157bcb53ba24def4db79caf60
Michael Fuget <michaelfuget@gmail.com>
2021-02-02 01:56:28 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function WakeDog(dogName, dogBreed)
+{
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+function leashDog(dogName, dogBreed)
+{
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+function WakeDog(dogName, dogBreed)
+{
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+function WakeDog(dogName, dogBreed)
+{
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+function WakeDog(dogName, dogBreed)
+{
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+function WakeDog(dogName, dogBreed)
+{
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
