0b2a58af444caab321a72addbd8eae6b2e7fe44c
Michael Fuget <michaelfuget@gmail.com>
2021-02-02 02:02:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 function wakeDog(dogName, dogBreed)
 {
   var out = `Wake ${dogName} the ${dogBreed}`;
+  return out
 }
 function leashDog(dogName, dogBreed)
 {
   var out = `leash ${dogName} the ${dogBreed}`
+  return out
+
 }
 function walkToPark(dogName, dogBreed)
 {
   var out = `Walk to the park with ${dogName} the ${dogBreed}`
+  return out
+
 }
 function throwFrisbee(dogName, dogBreed)
 {
   var out = `Throw the frisbee for ${dogName} the ${dogBreed}`
+  return out
+
 }
 function walkHome(dogName, dogBreed)
 {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+  var out = `Walk home with ${dogName} the ${dogBreed}`
+  return out
+
 }
 function unleashDog(dogName, dogBreed)
 {
